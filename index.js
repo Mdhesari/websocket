9486f44b01ce02715990fecd69af57659ddea8dc
Mohamad Fazel <mdhesari99@gmail.com>
2019-09-15 03:40:32 +0430

    implement primary functinality and show messeges

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ let server = app.listen(4000, () => {
   console.log("Listenning to requests on port 4000");
 });
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-let io = socket(server)
+let io = socket(server);
 
 io.on("connection", socket => {
-  console.log("the connection was made", socket.id)
-})
+  console.log("the connection was made", socket.id);
+
+  socket.on("chat", data => {
+    io.sockets.emit("chat", data);
+  });
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,19 @@
 // in the name of allah
-socket = io.connect('http://localhost:4000')
+let socket = io.connect("http://localhost:4000");
+
+let output = document.getElementById("output"),
+  handle = document.getElementById("handle"),
+  message = document.getElementById("message"),
+  btn = document.getElementById("btn");
+
+btn.addEventListener("click", e => {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+socket.on("chat", data => {
+  output.innerHTML +=
+    "<p><strong> " + data.handle + " </strong> " + data.message + "</p>";
+});
